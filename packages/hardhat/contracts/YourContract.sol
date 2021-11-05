841caddc72e57df5b92edd41bb1227912d103b4b
tomo <tomo@crowdeo.com>
2021-11-05 13:27:23 +0700

    Demonstrate how we can use external Uniswap contract in our own

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -10,6 +10,10 @@ contract YourContract {
 
   string public purpose = "Building Unstoppable Apps!!!";
 
+  address public token0DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+  address public token1MAI = 0xa3Fa99A148fA48D14Ed51d610c367C61876997F1;
+  address public pairDAIMAI = 0x74214F5d8AA71b8dc921D8A963a1Ba3605050781;
+
   constructor() {
     // what should we do on deploy?
   }
@@ -19,4 +23,22 @@ contract YourContract {
       console.log(msg.sender,"set purpose to",purpose);
       //emit SetPurpose(msg.sender, purpose);
   }
+
+  function myName(address addr) external pure returns (string memory) {
+    MyPair pair = MyPair(addr);
+    return pair.name();
+  }
+  function getAddrReserves(address addr) external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
+    MyPair pair = MyPair(addr);
+    return pair.getReserves();
+  }
+  function getDAIMAIReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) {
+    MyPair pair = MyPair(pairDAIMAI);
+    return pair.getReserves();
+  }
+}
+
+interface MyPair {
+  function name() external pure returns (string memory);
+  function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
 }
